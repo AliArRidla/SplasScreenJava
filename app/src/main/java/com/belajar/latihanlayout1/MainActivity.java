af1784b9f4e5eb11840f5a97bc1cf26ec17db802
zero0dayali <beeunik0@gmail.com>
2021-02-04 08:10:48 +0700

    menambah halaman login

--- app/src/main/java/com/belajar/latihanlayout1/MainActivity.java
+++ app/src/main/java/com/belajar/latihanlayout1/MainActivity.java
@@ -2,9 +2,13 @@ package com.belajar.latihanlayout1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -41,8 +45,13 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this,Dashboard.class);
-                startActivity(intent);
+                Intent intent = new Intent(MainActivity.this,Login.class);
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View,String>(image,"Logo_ image");
+                pairs[1] = new Pair<View,String>(logo ,"Logo_ image");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                startActivity(intent,options.toBundle());
                 finish();
             }
         },SPLASH_SCREEN);
